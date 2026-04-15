40ef4b1163bfa3b34a7f57973da1dcfda696aefa
thomasEFox <thomasefox101@proton.me>
2026-04-14 20:22:07 -0400

    2.1.12

--- communicators/state.py
+++ communicators/state.py
@@ -78,7 +78,7 @@ class Manifest:
                     if line and not line.startswith('#'):
                         ignored.add(line)
         dirs = [Path(__file__).parent.parent / 'Communicators/communicators/', Path(__file__).parent.parent / 'Communicators/']
-        print("gitignore_path", gitignore_path)
+        print("dirs", dirs)
         files = set()
         for d in dirs:
             if d.exists():
