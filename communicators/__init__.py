82c21c803850b6d3e7e4b530dbe7655b885ed8a5
thomasEFox <thomasefox101@proton.me>
2026-04-20 09:49:55 -0400

    2.1.43

--- communicators/__init__.py
+++ communicators/__init__.py
@@ -1,5 +1,5 @@
 from .decorators import server, tank, NegativeCommunicator, PositiveCommunicator
-from .state import manifest, truncate, freight, singleton
+from .state import manifest, truncate, freight, singleton, anchor_multiton, aux_multiton
 from .unix_socket import unix_client, unix_server
 
-__all__ = ['server', 'tank', 'NegativeCommunicator', 'PositiveCommunicator', 'singleton', 'manifest', 'truncate', 'freight', 'unix_client', 'unix_server']
+__all__ = ['server', 'tank', 'NegativeCommunicator', 'PositiveCommunicator', 'singleton', 'anchor_multiton', 'aux_multiton', 'manifest', 'truncate', 'freight', 'unix_client', 'unix_server']
